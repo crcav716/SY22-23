aafc9aea9323beadfde5d33b6d3b4b9ed0ebddd2
Chloe Cavalluzzi <crcav716@learner.dvusd.org>
2022-08-22 07:57:37 -0700

    Monday Update

---
+++ VB/Chocolate Dex/Chocolate Dex/Form1.Designer.vb
@@ -0,0 +1,319 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ChocolateButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.nameLabel = New System.Windows.Forms.Label()
+        Me.shapesLabel = New System.Windows.Forms.Label()
+        Me.flavorsLabel = New System.Windows.Forms.Label()
+        Me.darkchocolateButton = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.orginatedfromLabel = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.bestbrandLabel = New System.Windows.Forms.Label()
+        Me.milkchocolateButton = New System.Windows.Forms.Button()
+        Me.semisweetchocolateButton = New System.Windows.Forms.Button()
+        Me.sweetgermanButton = New System.Windows.Forms.Button()
+        Me.sweetgermanchocolatePictureBox = New System.Windows.Forms.PictureBox()
+        Me.semisweetchocolatePictureBox = New System.Windows.Forms.PictureBox()
+        Me.milkchocolatePictureBox = New System.Windows.Forms.PictureBox()
+        Me.darkchocolatePictureBox = New System.Windows.Forms.PictureBox()
+        Me.ChocolatePictureBox = New System.Windows.Forms.PictureBox()
+        Me.displayPictureBox = New System.Windows.Forms.PictureBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.sweetgermanchocolatePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.semisweetchocolatePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.milkchocolatePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.darkchocolatePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ChocolatePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.displayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ChocolateButton
+        '
+        Me.ChocolateButton.Location = New System.Drawing.Point(488, 275)
+        Me.ChocolateButton.Name = "ChocolateButton"
+        Me.ChocolateButton.Size = New System.Drawing.Size(71, 23)
+        Me.ChocolateButton.TabIndex = 0
+        Me.ChocolateButton.Text = " White "
+        Me.ChocolateButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(364, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(363, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Shapes:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(363, 80)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Flavors:"
+        '
+        'nameLabel
+        '
+        Me.nameLabel.AutoSize = True
+        Me.nameLabel.Location = New System.Drawing.Point(426, 20)
+        Me.nameLabel.Name = "nameLabel"
+        Me.nameLabel.Size = New System.Drawing.Size(0, 16)
+        Me.nameLabel.TabIndex = 6
+        '
+        'shapesLabel
+        '
+        Me.shapesLabel.AutoSize = True
+        Me.shapesLabel.Location = New System.Drawing.Point(426, 49)
+        Me.shapesLabel.Name = "shapesLabel"
+        Me.shapesLabel.Size = New System.Drawing.Size(0, 16)
+        Me.shapesLabel.TabIndex = 7
+        '
+        'flavorsLabel
+        '
+        Me.flavorsLabel.AutoSize = True
+        Me.flavorsLabel.Location = New System.Drawing.Point(426, 80)
+        Me.flavorsLabel.Name = "flavorsLabel"
+        Me.flavorsLabel.Size = New System.Drawing.Size(0, 16)
+        Me.flavorsLabel.TabIndex = 8
+        '
+        'darkchocolateButton
+        '
+        Me.darkchocolateButton.Location = New System.Drawing.Point(779, 275)
+        Me.darkchocolateButton.Name = "darkchocolateButton"
+        Me.darkchocolateButton.Size = New System.Drawing.Size(73, 23)
+        Me.darkchocolateButton.TabIndex = 9
+        Me.darkchocolateButton.Text = " Dark "
+        Me.darkchocolateButton.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(363, 109)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(106, 16)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Originated From:"
+        '
+        'orginatedfromLabel
+        '
+        Me.orginatedfromLabel.AutoSize = True
+        Me.orginatedfromLabel.Location = New System.Drawing.Point(476, 109)
+        Me.orginatedfromLabel.Name = "orginatedfromLabel"
+        Me.orginatedfromLabel.Size = New System.Drawing.Size(0, 16)
+        Me.orginatedfromLabel.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(365, 138)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(76, 16)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Best Brand:"
+        '
+        'bestbrandLabel
+        '
+        Me.bestbrandLabel.AutoSize = True
+        Me.bestbrandLabel.Location = New System.Drawing.Point(448, 138)
+        Me.bestbrandLabel.Name = "bestbrandLabel"
+        Me.bestbrandLabel.Size = New System.Drawing.Size(0, 16)
+        Me.bestbrandLabel.TabIndex = 14
+        '
+        'milkchocolateButton
+        '
+        Me.milkchocolateButton.Location = New System.Drawing.Point(412, 275)
+        Me.milkchocolateButton.Name = "milkchocolateButton"
+        Me.milkchocolateButton.Size = New System.Drawing.Size(70, 23)
+        Me.milkchocolateButton.TabIndex = 15
+        Me.milkchocolateButton.Text = "Milk "
+        Me.milkchocolateButton.UseVisualStyleBackColor = True
+        '
+        'semisweetchocolateButton
+        '
+        Me.semisweetchocolateButton.Location = New System.Drawing.Point(678, 275)
+        Me.semisweetchocolateButton.Name = "semisweetchocolateButton"
+        Me.semisweetchocolateButton.Size = New System.Drawing.Size(95, 23)
+        Me.semisweetchocolateButton.TabIndex = 17
+        Me.semisweetchocolateButton.Text = "Semisweet "
+        Me.semisweetchocolateButton.UseVisualStyleBackColor = True
+        '
+        'sweetgermanButton
+        '
+        Me.sweetgermanButton.Location = New System.Drawing.Point(565, 275)
+        Me.sweetgermanButton.Name = "sweetgermanButton"
+        Me.sweetgermanButton.Size = New System.Drawing.Size(107, 23)
+        Me.sweetgermanButton.TabIndex = 19
+        Me.sweetgermanButton.Text = "Sweet German"
+        Me.sweetgermanButton.UseVisualStyleBackColor = True
+        '
+        'sweetgermanchocolatePictureBox
+        '
+        Me.sweetgermanchocolatePictureBox.Image = Global.Chocolate_Dex.My.Resources.Resources.download__3_
+        Me.sweetgermanchocolatePictureBox.Location = New System.Drawing.Point(389, 359)
+        Me.sweetgermanchocolatePictureBox.Name = "sweetgermanchocolatePictureBox"
+        Me.sweetgermanchocolatePictureBox.Size = New System.Drawing.Size(156, 50)
+        Me.sweetgermanchocolatePictureBox.TabIndex = 20
+        Me.sweetgermanchocolatePictureBox.TabStop = False
+        Me.sweetgermanchocolatePictureBox.Visible = False
+        '
+        'semisweetchocolatePictureBox
+        '
+        Me.semisweetchocolatePictureBox.Image = Global.Chocolate_Dex.My.Resources.Resources.download__2_
+        Me.semisweetchocolatePictureBox.Location = New System.Drawing.Point(573, 359)
+        Me.semisweetchocolatePictureBox.Name = "semisweetchocolatePictureBox"
+        Me.semisweetchocolatePictureBox.Size = New System.Drawing.Size(155, 50)
+        Me.semisweetchocolatePictureBox.TabIndex = 18
+        Me.semisweetchocolatePictureBox.TabStop = False
+        Me.semisweetchocolatePictureBox.Visible = False
+        '
+        'milkchocolatePictureBox
+        '
+        Me.milkchocolatePictureBox.Image = Global.Chocolate_Dex.My.Resources.Resources.images
+        Me.milkchocolatePictureBox.Location = New System.Drawing.Point(204, 359)
+        Me.milkchocolatePictureBox.Name = "milkchocolatePictureBox"
+        Me.milkchocolatePictureBox.Size = New System.Drawing.Size(154, 50)
+        Me.milkchocolatePictureBox.TabIndex = 16
+        Me.milkchocolatePictureBox.TabStop = False
+        Me.milkchocolatePictureBox.Visible = False
+        '
+        'darkchocolatePictureBox
+        '
+        Me.darkchocolatePictureBox.Image = Global.Chocolate_Dex.My.Resources.Resources.download2
+        Me.darkchocolatePictureBox.Location = New System.Drawing.Point(757, 359)
+        Me.darkchocolatePictureBox.Name = "darkchocolatePictureBox"
+        Me.darkchocolatePictureBox.Size = New System.Drawing.Size(187, 50)
+        Me.darkchocolatePictureBox.TabIndex = 10
+        Me.darkchocolatePictureBox.TabStop = False
+        Me.darkchocolatePictureBox.Visible = False
+        '
+        'ChocolatePictureBox
+        '
+        Me.ChocolatePictureBox.Image = Global.Chocolate_Dex.My.Resources.Resources.download
+        Me.ChocolatePictureBox.Location = New System.Drawing.Point(23, 359)
+        Me.ChocolatePictureBox.Name = "ChocolatePictureBox"
+        Me.ChocolatePictureBox.Size = New System.Drawing.Size(150, 50)
+        Me.ChocolatePictureBox.TabIndex = 2
+        Me.ChocolatePictureBox.TabStop = False
+        Me.ChocolatePictureBox.Visible = False
+        '
+        'displayPictureBox
+        '
+        Me.displayPictureBox.BackgroundImage = Global.Chocolate_Dex.My.Resources.Resources.download__4_
+        Me.displayPictureBox.Location = New System.Drawing.Point(0, -3)
+        Me.displayPictureBox.Name = "displayPictureBox"
+        Me.displayPictureBox.Size = New System.Drawing.Size(359, 204)
+        Me.displayPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.displayPictureBox.TabIndex = 1
+        Me.displayPictureBox.TabStop = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Palatino Linotype", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label6.Location = New System.Drawing.Point(-7, 216)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(369, 38)
+        Me.Label6.TabIndex = 21
+        Me.Label6.Text = "Chloe's Favorite Chocolates"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Chocolate_Dex.My.Resources.Resources.download__1_
+        Me.ClientSize = New System.Drawing.Size(936, 314)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.sweetgermanchocolatePictureBox)
+        Me.Controls.Add(Me.sweetgermanButton)
+        Me.Controls.Add(Me.semisweetchocolatePictureBox)
+        Me.Controls.Add(Me.semisweetchocolateButton)
+        Me.Controls.Add(Me.milkchocolatePictureBox)
+        Me.Controls.Add(Me.milkchocolateButton)
+        Me.Controls.Add(Me.bestbrandLabel)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.orginatedfromLabel)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.darkchocolatePictureBox)
+        Me.Controls.Add(Me.darkchocolateButton)
+        Me.Controls.Add(Me.flavorsLabel)
+        Me.Controls.Add(Me.shapesLabel)
+        Me.Controls.Add(Me.nameLabel)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ChocolatePictureBox)
+        Me.Controls.Add(Me.displayPictureBox)
+        Me.Controls.Add(Me.ChocolateButton)
+        Me.Name = "Form1"
+        Me.Text = "ChocolateDex"
+        CType(Me.sweetgermanchocolatePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.semisweetchocolatePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.milkchocolatePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.darkchocolatePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ChocolatePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.displayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ChocolateButton As Button
+    Friend WithEvents displayPictureBox As PictureBox
+    Friend WithEvents ChocolatePictureBox As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents nameLabel As Label
+    Friend WithEvents shapesLabel As Label
+    Friend WithEvents flavorsLabel As Label
+    Friend WithEvents darkchocolatePictureBox As PictureBox
+    Friend WithEvents darkchocolateButton As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents orginatedfromLabel As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents bestbrandLabel As Label
+    Friend WithEvents milkchocolateButton As Button
+    Friend WithEvents milkchocolatePictureBox As PictureBox
+    Friend WithEvents semisweetchocolateButton As Button
+    Friend WithEvents semisweetchocolatePictureBox As PictureBox
+    Friend WithEvents sweetgermanButton As Button
+    Friend WithEvents sweetgermanchocolatePictureBox As PictureBox
+    Friend WithEvents Label6 As Label
+End Class
---
+++ VB/Chocolate Dex/Chocolate Dex/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Private Sub ChocolatePictureButton_Click(sender As Object, e As EventArgs) Handles ChocolateButton.Click
+        displayPictureBox.Image = ChocolatePictureBox.Image
+        nameLabel.Text = "White Chocolate"
+        shapesLabel.Text = "Circle"
+        flavorsLabel.Text = "Milky"
+        orginatedfromLabel.Text = "Switzerland"
+        bestbrandLabel.Text = "Ghirardelli Chocolate Company"
+    End Sub
+
+    Private Sub darkchocolateButton_Click(sender As Object, e As EventArgs) Handles darkchocolateButton.Click
+        displayPictureBox.Image = darkchocolatePictureBox.Image
+        nameLabel.Text = "Dark Chocolate"
+        shapesLabel.Text = "Square"
+        flavorsLabel.Text = "Bitter"
+        orginatedfromLabel.Text = "Mexico/South America"
+        bestbrandLabel.Text = "Lindt Chocolate"
+
+    End Sub
+
+    Private Sub milkchocolateButton_Click(sender As Object, e As EventArgs) Handles milkchocolateButton.Click
+        displayPictureBox.Image = milkchocolatePictureBox.Image
+        nameLabel.Text = "Milk Chocolate"
+        shapesLabel.Text = "Rectangle"
+        flavorsLabel.Text = "Sweet and Chocolatey"
+        orginatedfromLabel.Text = "Switzerland"
+        bestbrandLabel.Text = "Hersey Chocolate Company"
+    End Sub
+
+    Private Sub semisweetchocolateButton_Click(sender As Object, e As EventArgs) Handles semisweetchocolateButton.Click
+        displayPictureBox.Image = semisweetchocolatePictureBox.Image
+        nameLabel.Text = "Semisweet Chocolate"
+        shapesLabel.Text = "Round"
+        flavorsLabel.Text = "Bitter"
+        orginatedfromLabel.Text = "United States"
+        bestbrandLabel.Text = "Ghirardelli Semi-Sweet Premium Chocolate Chips"
+    End Sub
+
+    Private Sub sweetgermanButton_Click(sender As Object, e As EventArgs) Handles sweetgermanButton.Click
+        displayPictureBox.Image = sweetgermanchocolatePictureBox.Image
+        nameLabel.Text = "Sweet German Chocolate"
+        shapesLabel.Text = "Cubes"
+        flavorsLabel.Text = "Sweet"
+        orginatedfromLabel.Text = "United States"
+        bestbrandLabel.Text = "Ritter Sport"
+    End Sub
+
+
+End Class
